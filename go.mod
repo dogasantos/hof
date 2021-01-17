63ec435d66d97509561df74126781771fe79ef79
dogasantos <douglas.secco@gmail.com>
2021-01-17 14:03:28 -0300

    first blood new structure

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/dogasantos/hof
+
+go 1.15
+
+require (
+	github.com/bobesa/go-domain-util v0.0.0-20190911083921-4033b5f7dd89
+	github.com/likexian/whois-go v1.7.2
+	github.com/likexian/whois-parser-go v1.15.2
+	github.com/miekg/dns v1.1.35
+	github.com/projectdiscovery/retryabledns v1.0.5
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"os"
+	"github.com/dogasantos/hof/hof"
+)
+
+const banner = `
+ _               _                                            __ _ _ _
+| |__   ___  ___| |_       _____      ___ __   ___ _ __      / _(_) | |_ ___ _ __
+| '_ \ / _ \/ __| __|____ / _ \ \ /\ / / '_ \ / _ \ '__|____| |_| | | __/ _ \ '__|
+| | | | (_) \__ \ ||_____| (_) \ V  V /| | | |  __/ | |_____|  _| | | ||  __/ |
+|_| |_|\___/|___/\__|     \___/ \_/\_/ |_| |_|\___|_|       |_| |_|_|\__\___|_|
+==================================================================================
+`
+const Version = `0.1`
+
+func showBanner() {
+	fmt.Printf("%s", banner)
+	fmt.Printf("\t\t\t\t\t\t\t\tversion: %s\n\n",Version)
+}
+
+func main() {	
+	gpath := os.Getenv("GOPATH")
+	ProjectDir := "/src/githuib.com/dogasantos/host-owner-filter/"
+	soaKbFile := "soakb.json"
+	defaultConfig := gpath + ProjectDir + soaKbFile
+	
+	options := hof.Options{}
+	
+	flag.StringVar(&options.Domainf, 		"K", "", "List of known domains to serve as a seed to compare")
+	flag.StringVar(&options.Hosts, 			"L", "", "File input with list of subdomains")
+	flag.StringVar(&options.OutputFile, 	"o", "", "File to write output to (optional)")
+	flag.StringVar(&options.SoaKbFile, 		"s", defaultConfig, "Soa servers to filter out (a default one is provided if not set)")
+	flag.BoolVar(&options.Verbose, 			"verbose", false, "Verbose output")
+
+	flag.Parse()
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	hof.Process(&options)
+
+}
